9b4059cfb70e64c22f15fcc9b7dedcb0302ea698
KaidoSo <kaido.soomlais@gmail.com>
2021-05-13 16:37:45 +0300

    added code to display thumbnails

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
         var res = JSON.parse(xhttp.responseText);
-        console.log(res.items.map(function(item){
-            return(item.snippet)
+        var videoData = (res.items.map(function(item){
+            return item.snippet
         }));
+
+        var container = document.querySelector('#video-divs');
+        videoData.forEach(function(video){
+            console.log(video);
+            var videoDiv = document.createElement('div');
+            videoDiv.innerHTML = `
+                <img src=${video.thumbnails.default.url}>
+            `;
+            container.appendChild(videoDiv);
+        })
     }
 };
 xhttp.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&q=surfing&key=" + key, true);
