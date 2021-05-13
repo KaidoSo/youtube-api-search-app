d7147e8663658aa525c9b2cc658ec02b5e4850df
KaidoSo <kaido.soomlais@gmail.com>
2021-05-13 16:30:18 +0300

    added code to retrieve snippet arrays

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
-        document.getElementById("res").innerHTML = xhttp.responseText;
+        var res = JSON.parse(xhttp.responseText);
+        console.log(res.items.map(function(item){
+            return(item.snippet)
+        }));
     }
 };
 xhttp.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&q=surfing&key=" + key, true);
